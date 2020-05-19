926e7da580795c0b40afec221a659f0b2860c582
AjeshRPai <ajeshr2k1@gmail.com>
2020-05-19 12:35:03 +0530

    [+] Kotlin Coroutine Examples Added

--- src/main/kotlin/Coroutines/Coroutines.kt
+++ src/main/kotlin/Coroutines/Coroutines.kt
@@ -1,20 +1,14 @@
 package Coroutines
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 object Coroutines {
 
     @JvmStatic
     fun main(args: Array<String>) {
-
-        testFunction1()
-        Thread.sleep(1000)
-        testRunFunction1()
-        testRunFunction2()
-        testRunFunction3()
+        //runBlockingTest1()
+//        launchWithoutContext()
+        launchWithContext()
     }
 
     fun testFunction1(){
@@ -26,6 +20,186 @@ object Coroutines {
     suspend fun square(i: Int): Int {
         delay(500)
         return i * i
+    }
+
+    fun ThreadVsRunSameThread(){
+        /***
+         *  Here Thread is Started with Run Command and it runs in the Same Thread
+         *  Both the thread and Run executes sequentially and on the same Thread
+         *
+         */
+        Thread {
+            println("-Thread--- start : ${getThreadName()}")
+            Thread.sleep(1000)
+            println(" -Thread--- ended : ${getThreadName()}")
+        }.run()
+
+        run {
+            println(" -Run------ start : ${getThreadName()}")
+            Thread.sleep(200)
+            println(" -Run------ ended : ${getThreadName()}")
+        }
+    }
+
+    fun ThreadVsRunDifferentThread() {
+        /**
+         * Here the Thread and the Run command is executed Parallely in different threads
+         * as the Start Command is used to start the Thread
+         *
+         * */
+
+        Thread {
+            println("-Thread--- start : ${getThreadName()}")
+            Thread.sleep(1000)
+            println("-Thread--- ended : ${getThreadName()}")
+        }.start()
+
+        run {
+            println("-Run------ start : ${getThreadName()}")
+            Thread.sleep(200)
+            println("-Run------ ended : ${getThreadName()}")
+        }
+
+    }
+
+    fun runBlockingTest1() {
+        runBlocking {
+            println("-Blocking- start : ${getThreadName()}")
+            Thread.sleep(1000)
+            println("-Blocking- ended : ${getThreadName()}")
+        }
+
+        run {
+            println("-Run------ start : ${getThreadName()}")
+            Thread.sleep(200)
+            println("-Run------ ended : ${getThreadName()}")
+        }
+    }
+
+    fun runBlockingTest2() {
+        runBlocking(Dispatchers.Default) {
+            println("-Blocking- start : ${getThreadName()}")
+            Thread.sleep(1000)
+            println("-Blocking- ended : ${getThreadName()}")
+        }
+
+        run {
+            println("-Run------ start : ${getThreadName()}")
+            Thread.sleep(200)
+            println("-Run------ ended : ${getThreadName()}")
+        }
+    }
+
+    fun launchWithoutContext() {
+        GlobalScope.launch {
+            println("--Launch-- start : ${getThreadName()}")
+            Thread.sleep(300)
+            println("--Launch-- ended : ${getThreadName()}")
+        }
+
+        run {
+            println("--Run----- start : ${getThreadName()}")
+            Thread.sleep(200)
+            println("--Run----- ended : ${getThreadName()}")
+        }
+    }
+
+    fun launchWithContext() {
+        runBlocking {
+            launch(coroutineContext) {
+                println("--Launch-- start : ${getThreadName()}")
+                Thread.sleep(1000)
+                println("--Launch-- ended : ${getThreadName()}")
+            }
+
+            run {
+                println("--Run----- start : ${getThreadName()}")
+                Thread.sleep(200)
+                println("--Run----- ended : ${getThreadName()}")
+            }
+        }
+    }
+
+    fun launchWithJoin() {
+        runBlocking {
+            launch(coroutineContext) {
+                println("--Launch-- start : ${getThreadName()}")
+                Thread.sleep(1000)
+                println("--Launch-- ended : ${getThreadName()}")
+            }.join()
+
+            run {
+                println("--Run----- start : ${getThreadName()}")
+                Thread.sleep(200)
+                println("--Run----- ended : ${getThreadName()}")
+            }
+        }
+    }
+
+    fun launchWithJoinLater() {
+        runBlocking {
+            val job = launch(coroutineContext) {
+                println("--Launch-- start : ${getThreadName()}")
+                Thread.sleep(1000)
+                println("--Launch-- ended : ${getThreadName()}")
+            }
+
+            run {
+                println("--Run----- start : ${getThreadName()}")
+                Thread.sleep(200)
+                job.join()
+                println("--Run----- ended : ${getThreadName()}")
+            }
+        }
+    }
+
+    fun launchWithDelay() {
+        runBlocking {
+            launch(coroutineContext) {
+                println("--Launch-- start : ${getThreadName()}")
+                delay(1000)
+                println("--Launch-- ended : ${getThreadName()}")
+            }
+
+            run {
+                println("--Run----- start : ${getThreadName()}")
+                delay(200)
+                println("--Run----- ended : ${getThreadName()}")
+            }
+        }
+    }
+
+    fun launchWithDelayJoin() {
+        runBlocking {
+            launch(coroutineContext) {
+                println("--Launch-- start : ${getThreadName()}")
+                delay(1000)
+                println("--Launch-- ended : ${getThreadName()}")
+            }.join()
+
+            run {
+                println("--Run----- start : ${getThreadName()}")
+                delay(200)
+                println("--Run----- ended : ${getThreadName()}")
+            }
+        }
+    }
+
+    fun launchWithJob() {
+        runBlocking {
+            val job = launch(coroutineContext) {
+                println("--Launch-- start : ${getThreadName()}")
+                delay(1000)
+                println("--Launch-- ended : ${getThreadName()}")
+            }
+
+            run {
+                println("--Run----- start : ${getThreadName()}")
+                delay(200)
+                job.cancel()
+                println("--Run----- ended : ${getThreadName()}")
+            }
+        }
     }
 
     fun testRunFunction1() {
@@ -46,6 +220,7 @@ object Coroutines {
 
     fun testRunFunction2() {
         //uses Thread.sleep to show the Blocked behaviour
+        // Thus is run inside same Thread
         runBlocking {
             // Start a coroutine
             launch(coroutineContext) {
@@ -63,7 +238,14 @@ object Coroutines {
     }
 
     fun testRunFunction3() {
-        //uses delay to show the Suspended Behaviour
+        // uses delay to show the Suspended Behaviour
+        // This is run inside the same Thread
+        // When Delay is used the Function is Suspended and the Thread Goes back to pool
+        // So in this case the run is executed first and then delay suspends the Thread but doesnt block the thread
+        // So launch starts and suspends the Thread again and then run ends since the delay is already finished
+        // Then launch ends
+        // In which case launch and run will run concurrently
+
         runBlocking {
             // Start a coroutine
             launch(coroutineContext) {
