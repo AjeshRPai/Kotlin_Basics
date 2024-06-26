a65e05fb3d4d644683446de6309bda1d9b790362
Ajesh R Pai <ajeshr2k1@gmail.com>
2024-06-26 14:45:59 +0530

    + Adds: the basics of kotlin coroutines from book

---
+++ src/main/kotlin/Coroutines/Basics.kt
@@ -0,0 +1,16 @@
+package Coroutines
+
+import kotlinx.coroutines.suspendCancellableCoroutine
+
+
+suspend fun main() {
+    val i: Int = suspendCancellableCoroutine<Int> { c ->
+        c.resumeWith(Result.success(42))
+    }
+    println(i) // 42
+    val str: String = suspendCancellableCoroutine<String> { c -> c.resumeWith(Result.success("Some text")) }
+    println(str) // Some text
+    val b: Boolean = suspendCancellableCoroutine<Boolean> { c -> c.resumeWith(Result.success(true))
+    }
+    println(b) // true
+}
