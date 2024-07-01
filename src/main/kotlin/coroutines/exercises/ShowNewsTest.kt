42380c2c78ca3081eccb5a5aec75c1dbc8730acc
Ajesh R Pai <ajeshr2k1@gmail.com>
2024-07-01 14:07:28 +0530

    ↑ Updates: the logic to remove redundant code

--- src/main/kotlin/coroutines/exercises/ShowNewsTest.kt
+++ src/main/kotlin/coroutines/exercises/ShowNewsTest.kt
@@ -39,30 +39,3 @@ data class User(
 data class Friend(val id: String)
 data class Profile(val description: String)
 
-class TestUserDataRepository : UserDataRepository {
-    override suspend fun notifyProfileShown() {
-        delay(10000)
-    }
-
-    override suspend fun getName(): String {
-        delay(1000)
-        return "Ben"
-    }
-
-    override suspend fun getFriends(): List<Friend> {
-        delay(1000)
-        return listOf(Friend("some-friend-id-1"))
-    }
-
-    override suspend fun getProfile(): Profile {
-        delay(1000)
-        return Profile("Example description")
-    }
-}
-
-class TestUserDataView : UserDataView {
-    override fun show(user: User) {
-        print(user)
-    }
-}
-
