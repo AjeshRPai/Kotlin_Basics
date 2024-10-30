648f55210fc403c2750a94d07d43ca933a1892a3
Ajesh R Pai <ajeshr2k1@gmail.com>
2024-10-30 15:44:09 +0530

    Some examples on delegation

--- src/main/kotlin/Delegation/MainClassDelegation.kt
+++ src/main/kotlin/Delegation/MainClassDelegation.kt
@@ -1,45 +1,122 @@
 package Delegation
 
-import kotlin.collections.ArrayList
+import coroutines.channel.Coffee
+
+// example from advanced kotlin book
+interface Creature {
+    val attackPower: Int
+    val defensePower: Int
+    fun attack()
+}
+class GenericCreature(
+    override val attackPower: Int,
+    override val defensePower: Int,
+) : Creature {
+    override fun attack() {
+        println("Attacking with $attackPower")
+    }
+}
+class Goblin : Creature by GenericCreature(2, 1)
+
+fun example1() {
+    val goblin = Goblin()
+    println(goblin.defensePower) // 1
+    goblin.attack() // Attacking with 2
+}
 
 
-object MainClassDelegation {
-    @JvmStatic
-    fun main(args: Array<String>) {
+interface Instrument {
+    fun play()
+}
+
+class Guitar : Instrument {
+    override fun play() {
+        println("Strumming the guitar!")
     }
 }
 
-object apiSingleton{
-}
-
-interface ApiInterface {
-     fun getSomething(userid: String,token: String):List<MyData>
-}
-
-data class A(val apiInterface:A,
-             val userid:String,
-             val token:String,
-             val errorHandler: ErrorHandler): ApiInterface by apiInterface
-
-data class ErrorHandler(val defaultString: String,
-                            val sockettimeOutString:String?=defaultString,
-                            val timeOutError:String?=defaultString,
-                            val illegalStateString:String?=defaultString,
-                            val invalidData:String?=defaultString)
+class Guitarist : Instrument by Guitar() // Delegating directly to an instance of Guitar
 
 
-data class MyData(val name:String,val id:Int)
-
-private fun getDummyValues(): ArrayList<MyData> {
-    val values=ArrayList<MyData>(10)
-    for (i in 1..10){
-        values.add(MyData("name"+i,i))
+class Piano : Instrument {
+    override fun play() {
+        println("Playing the piano!")
     }
-    return values
 }
 
-sealed class Failure(val defaultString: String,
-                     val sockettimeOutString:String?=defaultString,
-                     val timeOutError:String?=defaultString,
-                     val illegalStateString:String?=defaultString,
-                     val invalidData:String?=defaultString)
+class Pianist(instrument: Instrument) : Instrument by instrument // Delegate passed through constructor
+
+class Musician(private val instrument: Instrument) {
+    fun perform() {
+        instrument.play()
+    }
+}
+
+val piano = Piano()
+class PianistFromOuter : Instrument by piano // Delegating to a variable from outer scope
+
+
+class GuitaristWithDefault(val instrument: Instrument = Guitar()) : Instrument by instrument // Default to Guitar
+
+
+fun example2() {
+    val guitar = Guitar()
+    val piano = Piano()
+
+    val guitarist = Musician(guitar)
+    val pianist = Musician(piano)
+
+    guitarist.perform() // Strumming the guitar!
+    pianist.perform()   // Playing the piano!
+
+
+    fun main() {
+        // Guitarist using a direct delegate
+        val guitarist = Guitarist()
+        guitarist.play() // Output: Strumming the guitar!
+
+        // Pianist using a constructor parameter
+        val pianist = Pianist(Guitar()) // Delegating to Guitar
+        pianist.play() // Output: Strumming the guitar!
+
+        // Pianist using an outer scope variable
+        val pianistFromOuter = PianistFromOuter()
+        pianistFromOuter.play() // Output: Playing the piano!
+
+        // Guitarist with a default parameter
+        val guitaristWithDefault = GuitaristWithDefault()
+        guitaristWithDefault.play() // Output: Strumming the guitar! (default)
+    }
+
+}
+
+
+interface Singer{
+    fun sing() {
+        println("singing")
+    }
+}
+
+interface Dancer {
+    fun dance() {
+        println("dancing")
+    }
+}
+
+class Vocalist(): Singer
+
+class BreakDancer(): Dancer
+
+
+class Artist(
+    val breakDancer: BreakDancer,
+    val vocalist: Vocalist
+): Singer by vocalist, Dancer by breakDancer
+
+
+fun main() {
+    val artist = Artist(BreakDancer(), Vocalist())
+    artist.sing()
+    artist.dance()
+}
+
